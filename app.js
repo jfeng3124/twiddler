ab0682378cb142d27f71aa3fcdb932c055681e7d
Jia <jiafeng3124@gmail.com>
2019-01-29 21:01:38 -0800

    formatting

--- app.js
+++ app.js
@@ -6,30 +6,30 @@ $(document).ready(function(){
   
 
   $('.button').on('click', function () {
-      
-      $tweets.html('');
       var index = streams.home.length - 1;
+      var date = streams.home[index].created_at + ''
+      var dateStr = jQuery.timeago(date.split(' ').slice(1, 5).join(' '));
 
-        while(index >= 0){
-          var tweet = streams.home[index];
-          var date = streams.home[index].created_at + ''
-          var dateStr = jQuery.timeago(date.split(' ').slice(1, 5).join(' '));
+      $tweets.html('');
 
-          var $button = $('<button></button>');
-              $button.addClass('user-button');
-              $button.text('@' + tweet.user);
-              $button.appendTo($tweets);
-
-          var $tweet = $('<div></div>');
-              $tweet.html(`${tweet.message} <br> ---  ${dateStr} <br><br>`)
-              $tweet.appendTo($tweets);
-          index --;
+      while(index >= 0){
+        var tweet = streams.home[index];
+        var $button = $('<button></button>');
+            $button.addClass('user-button');
+            $button.text('@' + tweet.user);
+            $button.appendTo($tweets);
+        var $tweet = $('<div></div>');
+            $tweet.html(tweet.message)
+            $tweet.appendTo($tweets);
+        var $date = $('<div></div>');
+            $date.addClass('date');
+            $date.html(dateStr + '<br><br>');
+            $date.appendTo($tweets);
+        index --;
         }
 
       $('.user-button').on("click", function() {
-        
         $timeline.html('');
-
         var name = $(this).text();
             name = name.slice(1);;
         var tweet = streams.users[name];
@@ -39,8 +39,12 @@ $(document).ready(function(){
                 $button.text('@' + name);
                 $button.appendTo($timeline);
             var $tweet = $('<div></div>');
-                $tweet.html(`${tweet[i].message} <br> --- ${dateStr} <br><br>`)
+                $tweet.html(tweet[i].message)
                 $tweet.appendTo($timeline);
+            var $date = $('<div></div>');
+                $date.addClass('date');
+                $date.html(dateStr + '<br><br>');
+                $date.appendTo($timeline);    
           }
       })
   })

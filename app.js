08c66d1afa59f57f4516418e239b8d571232769a
Jia <jiafeng3124@gmail.com>
2019-01-31 11:52:29 -0800

    refactoring

--- app.js
+++ app.js
@@ -4,74 +4,84 @@ $(document).ready(function(){
   var $timeline = $('.timeline');
   
 
-  // var initialTweets = function () {
-  //   $tweets.html('');
-  //   var index = streams.home.length - 1;
-  //   var date = streams.home[index].created_at + '';
-  //   var dateStr = date.split(' ').slice(1, 5).join(' ');
-  //   var time = jQuery.timeago(dateStr);
-  //   while(index >= 0){
-  //       var tweet = streams.home[index];
-  //       var $button = $('<button></button>');
-  //           $button.addClass('user-button');
-  //           $button.text('@' + tweet.user);
-  //           $button.appendTo($tweets);
-  //       var $tweet = $('<div></div>');
-  //           $tweet.text(tweet.message)
-  //           $tweet.appendTo($tweets);
-  //       var $date = $('<div></div>');
-  //           $date.addClass('date');
-  //           $date.html(' ' + time + '<br><br>');
-  //           $date.appendTo($tweets);
-  //       index --;
-  //   }
-  // }
-
-  // initialTweets();
-
-  $('.button').on('click', function () {
-
+  var loadTweets = function () {
     $tweets.html('');
     var index = streams.home.length - 1;
-    var date = streams.home[index].created_at + '';
-    var dateStr = jQuery.timeago(date.split(' ').slice(1, 5).join(' '));
-      while(index >= 0){
+    
+    while(index >= 0){
+        var date = jQuery.timeago(streams.home[index].created_at);
+     
         var tweet = streams.home[index];
-        var $button = $('<button></button>');
+        var $button = $('<button class =></button>');
             $button.addClass('user-button');
             $button.text('@' + tweet.user);
-            $button.appendTo($tweets);
         var $tweet = $('<div></div>');
             $tweet.text(tweet.message)
-            $tweet.appendTo($tweets);
         var $date = $('<div></div>');
             $date.addClass('date');
-            $date.html(' ' + dateStr + '<br><br>');
+            $date.html(' ' + date + '<br><br>');
+
+            $button.appendTo($tweets);
+            $tweet.appendTo($tweets);
             $date.appendTo($tweets);
         index --;
-  }
+    }
 
-      $('.user-button').on("click", function() {
+    $('.user-button').click(function() {
         $timeline.html('');
-        var name = $(this).text();
-            name = name.slice(1);;
+        var name = $(this).text().slice(1);
         var tweet = streams.users[name];
-
           for(var i = 0; i < tweet.length; i++) {
             var $button = $('<button></button>');
                 $button.text('@' + name);
-                $button.appendTo($timeline);
             var $tweet = $('<div></div>');
-                $tweet.text(tweet[i].message);
-                $tweet.appendTo($timeline);
+                $tweet.text(tweet[i].message);    
             var $date = $('<div></div>');
                 $date.addClass('date');
-                $date.html(' ' + dateStr + '<br><br>');
+                $date.html(' ' + date + '<br><br>');
+
+                $button.appendTo($timeline);
+                $tweet.appendTo($timeline);
                 $date.appendTo($timeline);    
           };
       });
-  });
+  }
 
+  loadTweets();
+  $('.load-tweets').click(function () {
+    loadTweets();
+  }) 
+
+ // $('.load-user-tweet').click(function(event) {
+ //  console.log('click')
+ //    e.preventDefault();
+ //    var form = document.getElementById('user-tweet');
+ //    var username = form.elements['user-name'].value;
+ //    var message = form.elements['user-tweet'].value;
+ //  if (streams.home.length > 7) {
+ //    streams.home = streams.home.slice(streams.home.length - 7);
+ //  }
+ //  writeTweet(message);
+ // })
+
+  // $('.load-user-tweet').click(function () {
+  //   $tweets.html('');
+  //   console.log('click')
+  //   var form = document.getElementById('user-tweet');
+  //   var userName = form.elements['user-name'].value;
+  //   var userTweet = form.elements['user-tweet'].value;
+  //   var $button = $('<button></button>');
+  //       $button.text = ('@' + userName);
+  //   var $tweet = $('<div></div>');
+  //       $tweet.text(userTweet);
+  //   var $date = $('<div></div>');
+  //       $date.addClass('date');
+  //       $date.html(' ' + date + '<br><br>');  
+
+  //   $button.appendTo($tweets);
+  //   $tweet.appendTo($tweets);
+  //   $date.appendTo($tweets);
+  // });
 });
 
 
